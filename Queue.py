ea3989042c6609b25f62d05196670359962930d6
Rogger G. Diaz <roleyder>
2020-05-25 10:15:35 -0400

    Format document

--- Queue.py
+++ Queue.py
@@ -1,13 +1,17 @@
 class Queue():
-	"""docstring for Queue"""
-	def __init__(self):
-		self.items = []
-	def is_empty(self):
-		return self.items  == []
-	def enqueue(self, item):
-		self.items.insert(0, item)
-	def dequeue(self):
-		return self.items.pop()
-	def size(self):
-		return len(self.items)
-	
+    """docstring for Queue"""
+
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return self.items == []
+
+    def enqueue(self, item):
+        self.items.insert(0, item)
+
+    def dequeue(self):
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
--- Stack.py
+++ Stack.py
@@ -1,15 +1,20 @@
 class Stack():
-	"""docstring for Stack"""
-	def __init__(self):
-		self.items = []
-	def push(self, item):
-		self.items.insert(0, item)
-	def is_emptly(self):
-		return self.items == []
-	def pop(self):
-		return self.items.pop(0)
-	def peek(self):
-		return self.items[0]
-	def size(self):
-		return len(self.items)
+    """docstring for Stack"""
 
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.insert(0, item)
+
+    def is_emptly(self):
+        return self.items == []
+
+    def pop(self):
+        return self.items.pop(0)
+
+    def peek(self):
+        return self.items[0]
+
+    def size(self):
+        return len(self.items)
